08fb00aaa7c255668a42aae6cc20e01a4e607549
rkMylang <55255865+rohitrk185@users.noreply.github.com>
2024-05-29 20:36:42 +0530

    project init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Room } from "./Room";
+import { CollaborativeApp } from "./CollaborativeApp";
+
+export default function Page() {
+  return (
+    <div className="h-[100vh] w-full flex justify-center items-center text-center">
+      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+    </div>
+  );
+}
